3e5004bdb85f3fe1e76a0a3741cfb2a872d76515
Marina Montez <mmontez81@gmail.com>
2017-01-23 19:38:26 +0000

    Create that was easy button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,5 +1,15 @@
 function sayThatWasEasy() {
-    var thatWasEasy = new Audio("that_was-easy.mp3")
+    var thatWasEasy = new Audio("that_was_easy.mp3");
+    thatWasEasy.play();
 }
 
-$(#easy).on("click", sayThatWasEasy);
+$("#easy").on("click", sayThatWasEasy);
+
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event) {
+    if (event.charCode == 32) {
+        var thatWasEasy = new Audio("that_was_easy.mp3");
+        $("#easy").trigger("click");
+    }
+}
