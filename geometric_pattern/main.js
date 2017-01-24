59e2c5c203418f8b5401bdfc2dc4e806e907ecb3
Marina Montez <mmontez81@gmail.com>
2017-01-24 19:55:11 +0000

    Aff colors to geometric_pattern.

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,6 +1,9 @@
 var NUM_CIRCLES = 12;
 var circleDiameter; 
-var circleRadius
+var circleRadius;
+var rVal;
+var gVal;
+var bVal; 
 
 function setup() {
     createCanvas(480, 600);
@@ -9,6 +12,10 @@ function setup() {
 }
 
 function draw() {
+  rVal = 255;
+  gVal = 0;
+  bVal = 0; 
+  
     var isShifted = false;
     
     var y = height;
@@ -30,5 +37,12 @@ function draw() {
 
     y = y - circleRadius;
     isShifted = !isShifted;
+    
+    rVal = rVal - 2;
+    gVal = gVal + 7;
+    bVal = bVal + 3
   }
 }
+
+fill(color(rVal,gVal,bVal);
+stroke(color(rVal,gVal,bVal));
